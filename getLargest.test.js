4b002f7a2501921d8c7c2cd96816244cff5f9975
agiskorska <skorskaga@gmail.com>
2022-07-12 11:04:06 +0100

    Refactor tests to use each

--- getLargest.test.js
+++ getLargest.test.js
@@ -1,14 +1,21 @@
 const getLargest = require('./getLargest');
+const { each } = require('jest-each');
 
 describe( "getLargest", () => {
-    
+
     test("It is a function", () => {
-        expect(typeof getLargest).toBe("function")
+        expect(typeof getLargest).toBe("function");
     })
 
-    test("It returns the largest number", () => {
-        const result = getLargest(1,2,3);
-        expect(result).toEqual(3);
+    describe("It handles normal inputs succesfully", () => {
+        each([
+            [[1,2,3], 3],
+            [[1000000000,2,3], 1000000000],
+            [[-1,-2,-3], -1],
+            [[0.1,0.02,0.003], 0.1],
+        ]).test("", (arr, expected) => {
+            expect(getLargest(arr).toEqual(expected));
+        })  
     })
 })
 
